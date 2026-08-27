610f71ae9fe80c2502c4609bb0d83ce1180ccf22
KristijanDrazetic <kristijan118@gmail.com>
2026-08-27 17:44:56 +0200

    Update orders.js

--- orders.js
+++ orders.js
@@ -30,7 +30,7 @@ function displayProducts(){
          
       if(dress.Available !== 0){
         
-  productsHTML = productsHTML +=`<div class ="add__product" id = "pp"><button class="addBtn" id = "addBtnReservation" type="button">Add to Cart</button></div></div>`
+  productsHTML = productsHTML +=`<div class ="add__product" id = "pp"><button class="addBtn" id = "addBtnReservation" title = "${dress.Name}" type="button">Add to Cart</button></div></div>`
 
       }else if(dress.Available === 0){
   productsHTML = productsHTML +=`<div class ="add__product" ><p>Out of Stock</p></div></div>`
@@ -83,209 +83,177 @@ if (sortDresses) {
    sortDresses.addEventListener("change", sortFunction);
 }
 
+function preventDoubleClicks(){
+
+  const now = Date.now();
+      if (now - lastClick < 700) return true; 
+      lastClick = now;
+
+}
+
+
+function dressAvailabilityDisplay(dressAvailable, btn){
+
+  let productContainer = btn.closest(".product__container")
+  if(!productContainer) return
+  let stockDisplayContainer = productContainer.querySelector("#stock__display")
+  stockDisplayContainer.innerHTML =   ` In Stock: ${dressAvailable - 1}`
+    
+}
+
+
+function numberDisplayItemsInCart(btn){
+ 
+  let productContainerCart = btn.closest(".product__container2")
+  if(!productContainerCart) return
+  let itemsFromCart = JSON.parse(localStorage.getItem("itemsInCart"))
+  let numberOfUniqueItemsInCart = itemsFromCart.filter(element => element.Name === btn.title).length;
+  let uniqueDressDisplayCart = productContainerCart.querySelector("#productAmountInCartDisplay")
+  uniqueDressDisplayCart.innerHTML = numberOfUniqueItemsInCart
+    
+}
+
+function dressOutOfStockReservation( btn){
+
+  let productContainer = btn.closest(".product__container")
+  if(!productContainer) return
+  let stockDisplayContainer = productContainer.querySelector("#pp")
+  stockDisplayContainer.innerHTML =   `<p> Out of stock</p>`
+    
+}
+
+function dressOutOfStockCart( btn){
+
+  let productContainerCart2 = btn.closest(".product__container2")
+  if(!productContainerCart2) return
+  let stockDisplayContainer2 = productContainerCart2.querySelector("#available")
+  stockDisplayContainer2.style.color = "red"
+  stockDisplayContainer2.style.display = "unset"
+   
+    
+}
+
+function dressOutOfStockCartHide( btn){
+
+  let productContainerCart2 = btn.closest(".product__container2")
+  if(!productContainerCart2) return
+  let stockDisplayContainer2 = productContainerCart2.querySelector("#available")
+  stockDisplayContainer2.style.display = "none"
+   
+}
+
+
+function displaySumInCart(){
+
+  if( document.querySelector(".order___sum")){
+  document.querySelector(".order___sum").innerHTML = `Total: ${localStorage.getItem("cartSum")},00$`
+    }
+
+}
+
+
+function addSumToCart(dressPrice){
+
+  let cartSum = localStorage.getItem("cartSum")
+  localStorage.setItem("cartSum", +cartSum + dressPrice)
+  displaySumInCart()
+ 
+}
+
+
+function updateCartIconQuantity() {
+  
+  if (cartQuantityDisplay && localStorage.getItem("itemsInCart") !== null && JSON.parse(localStorage.getItem("itemsInCart")).length !== 0) {
+    cartQuantityDisplay.innerHTML = `(${JSON.parse(localStorage.getItem("itemsInCart")).length})`;
+    cartQuantityDisplay.style.visibility = "visible";
+  }else if (localStorage.getItem("itemsInCart") !==null && localStorage.getItem("itemsInCart").length === 2  ){
+    cartQuantityDisplay.style.visibility = "hidden";
+  }
+
+}
+
+updateCartIconQuantity()
+
 
 
 export function addToCart(){
 
-  document.querySelectorAll(".addBtn").forEach(function(btn){
+ document.querySelectorAll(".addBtn").forEach(function(btn){
 
   btn.addEventListener("click" ,async function(event){
 
-    
+      let buttonClicked = btn
 
-  
-  function numberDisplayItemsInCart(){
- let numberDisplayItemsInCart = document.getElementById("productAmountInCartDisplay")
-  if(numberDisplayItemsInCart){
-  let itemsFromCart = JSON.parse(localStorage.getItem("itemsInCart"))
-  let numberOfUniqueItemsInCart = itemsFromCart.filter(element => element.Name === event.target.parentNode.parentNode.children[1].innerHTML.slice(7)).length;
-  event.target.parentNode.parentNode.children[5].children[1].innerHTML = numberOfUniqueItemsInCart
-
-  }
-}
-
-
-
-
-
-
-
-   let name = this.parentNode.parentNode.children[1].innerHTML.slice(7)
-   
-   if(await getitemAvailable(`${name}`) === 0){
-      this.parentNode.parentNode.children[6].innerHTML = `In Stock: ${await getitemAvailable(`${name}`)}`
-
-       if(event.target.parentNode.children[3] ){
-  event.target.parentNode.children[3].style.display = "unset"
-   event.target.parentNode.children[3].style.color = "red"
-
- }
-      
-      if(this.parentNode.parentNode.children[7].innerHTML){
-  this.parentNode.parentNode.children[7].innerHTML = `<p> Out of stock</p>`
+      if (preventDoubleClicks()) {
+        
+        return; 
       }
-      
 
-  let orderSumDisplay = document.querySelector(".order___sum")
-
-  if(orderSumDisplay){
-  document.querySelector(".order___sum").innerHTML = `Total: ${localStorage.getItem("cartSum")},00$`
-  }
+  let dressAvailabilityDatabase = await getitemAvailable(`${btn.title}`)
+  let dressPriceDatabase = await getitemPrice(`${btn.title}`)
+  let dressesDataBase = await getDataFromDatabase();
+  let obj = dressesDataBase.find(o => o.Name === `${btn.title}`);
 
 
-  numberDisplayItemsInCart()
-
-
-
-  
+  if (dressAvailabilityDatabase === 0){
     
-  
-  return
-   }
-   let dressesDataBase = await getDataFromDatabase()
-    let obj = dressesDataBase.find(o => o.Name === this.parentNode.parentNode.children[1].innerHTML.slice(7));
-    
+     dressAvailabilityDisplay(1, buttonClicked)
+     updateCartIconQuantity()
+     numberDisplayItemsInCart(buttonClicked)
+     displaySumInCart()
+     dressOutOfStockReservation(buttonClicked)
+     dressOutOfStockCart(buttonClicked)
+   
+    return
 
-     if(await getitemAvailable(`${obj.Name}`) !== 0){
-  
-
-
-  function checkDoubleClicks() {
- 
-  const now = Date.now();
-  
-  if (now - lastClick < 700){
-return; 
   } 
-  localStorage.setItem("cartSum", +localStorage.getItem("cartSum") + obj.Price)
+
+  
+  if(localStorage.getItem("itemsInCart") === null){
+
+    localStorage.setItem("itemsInCart", JSON.stringify([obj]))
+  
+  }else {
+
+    let itemsFromCartStorage = JSON.parse(localStorage.getItem("itemsInCart"))
     
-  let dressAvailability = dressesDataBase.find(dressesDataBase => dressesDataBase.Name === obj.Name);
-
-
-    if(localStorage.getItem("itemsInCart") !== null){
-let itemsFromCart = [...JSON.parse(localStorage.getItem("itemsInCart"))]
-
-itemsFromCart.push(obj)
-
-localStorage.setItem("itemsInCart", JSON.stringify(itemsFromCart))
-
- }else {
-  let itemsFromCart = []
-
-itemsFromCart.push(obj)
-
-localStorage.setItem("itemsInCart", JSON.stringify(itemsFromCart))
-
-
-
- }
-    reduceQuantityDirectly(`${name}`)
-  lastClick = now;
-  
-}
-
-checkDoubleClicks()
-  
+    let items = [...itemsFromCartStorage, obj]
     
+    localStorage.setItem("itemsInCart", JSON.stringify(items))
   }
-
-
-  let objName = obj.Name
-
-
- await getitemAvailable(`${objName}`)
-
  
-  this.parentNode.parentNode.children[6].innerHTML = `In Stock: ${await getitemAvailable(`${objName}`)}`
 
-  if(await getitemAvailable(`${objName}`) > 0 || await getitemAvailable(`${objName}`) === 0){
-
-  }
-
-  let orderSumDisplay = document.querySelector(".order___sum")
-  if(orderSumDisplay){
-  document.querySelector(".order___sum").innerHTML = `Total: ${localStorage.getItem("cartSum")},00$`
-  }
-
-  numberDisplayItemsInCart()
- 
-  let outOfStockDisplay = document.getElementById(".available")
-  if(orderSumDisplay){
-document.querySelector(".order___sum").innerHTML = `Total: ${localStorage.getItem("cartSum")},00$`
-  }
-
-let productNotAvailableDisplay = document.getElementById("available")
+  addSumToCart(dressPriceDatabase)
+  reduceQuantityDirectly(btn.title)
+  dressAvailabilityDisplay(dressAvailabilityDatabase, buttonClicked)
+  updateCartIconQuantity()
+  numberDisplayItemsInCart(buttonClicked)
   
- if(await getitemAvailable(`${objName}`) === 0 && productNotAvailableDisplay){
- 
-  event.target.parentNode.children[3].style.display = "unset"
-   event.target.parentNode.children[3].style.color = "red"
-
-    }else if(await getitemAvailable(`${objName}`) !== 0 && productNotAvailableDisplay){
-event.target.parentNode.children[3].style.display = "none"
-   
-    }
-cartQuantityDisplay.innerHTML = `(${JSON.parse(localStorage.getItem("itemsInCart")).length})`
-cartQuantityDisplay.style.visibility = "visible"
   })
-   
-  }) 
-
+    
+})
+  
 }
 
 
 
-export function removeFromCart(){
+function subtractSumFromCart(dressPrice, quantity){
+
+  let cartSum = localStorage.getItem("cartSum")
+  localStorage.setItem("cartSum", +cartSum - dressPrice * quantity)
+  displaySumInCart()
+ 
+}
 
 
-  
-  document.querySelectorAll(".removeItemInCartBtn, .removeBtn").forEach(function(btn){
 
-  btn.addEventListener("click" , function(event){
-
-    if(JSON.parse(localStorage.getItem("itemsInCart")) !== null ){
-  cartQuantityDisplay.innerHTML = `(${JSON.parse(localStorage.getItem("itemsInCart")).length})`
-    }
-  
-  let obj = dressesDataBase.find(o => o.Name === this.parentNode.parentNode.children[1].innerHTML.slice(7));
-
-  
-  let objName = obj.Name
+function ifEmptyCart(){
 
 
-  if(event.target.className === "removeBtn"){
+    if(localStorage.getItem("cartSum") === null || +localStorage.getItem("cartSum") === 0){
 
-  
-    let sameDressesInCart = JSON.parse(localStorage.getItem("itemsInCart")).filter(element => element.Name ===  event.target.parentNode.parentNode.children[1].innerHTML.slice(7));
-    let dressesFromDatabase = dressesDataBase
-    let dressAvailability = dressesFromDatabase.find(dressesFromDatabase => dressesFromDatabase.Name === objName);
-  
-    
-    localStorage.setItem("dressesInDatabase", JSON.stringify(dressesFromDatabase))
-    obj.Available += sameDressesInCart.length
-    
-    const target = event.target.parentNode.parentNode.children[1].innerHTML.slice(7)
-
-    let remainingDressesInCart = JSON.parse(localStorage.getItem("itemsInCart")).filter(item => item.Name !== target);
-
-  
-
-    localStorage.setItem("itemsInCart", JSON.stringify(remainingDressesInCart))
-
-    
-
-    localStorage.setItem("cartSum", +localStorage.getItem("cartSum") - obj.Price * sameDressesInCart.length)
-    
-    document.querySelector(".order___sum").innerHTML = `Total: ${localStorage.getItem("cartSum")},00$`
-    event.target.parentNode.parentNode.style.display ="none"
-
-    addQuantityDirectlyMultipleItems(`${objName}`, sameDressesInCart.length)
-
-
-    if(+localStorage.getItem("cartSum") === 0){
-  
-    
-    document.querySelector(".order___sum").innerHTML = ""
+      
+  document.querySelector(".order___sum").innerHTML = ""
 
     document.getElementById("hero2").innerHTML = `<p>It's pretty empty here.</p>
                                                     <a href="./reservation.html">
@@ -293,130 +261,89 @@ export function removeFromCart(){
 
     document.getElementById("checkoutBtn").style.display ="none"
     document.getElementById("cartQuantity").style.display ="none"
- 
-  
     }
-
-     
-    cartQuantityDisplay.innerHTML = `(${JSON.parse(localStorage.getItem("itemsInCart")).length})`
-
-    return
-  }
-
- 
-
- 
-  document.querySelector(".order___sum").innerHTML = `Total: ${localStorage.getItem("cartSum")},00$`
-
-  if(+localStorage.getItem("cartSum") === 0){
-  
-    
-    document.querySelector(".order___sum").innerHTML = ""
-
-    document.getElementById("hero2").innerHTML = `<p>It's pretty empty here.</p>
-                                                    <a href="./reservation.html">
-  Let yourself be inspired</a>`
-
-  document.getElementById("checkoutBtn").style.display ="none"
-  document.getElementById("cartQuantity").style.display ="none"
-  }
-
-
-
-  let itemsFromCart = JSON.parse(localStorage.getItem("itemsInCart"))
-
-
-  let dressIndex = itemsFromCart.findIndex(dress => dress.Name ===  event.target.parentNode.parentNode.children[1].innerHTML.slice(7) );
-
-
-
-  function numberOfUniqueItemsInCart(){
-  let numberOfUniqueItemsInCart = itemsFromCart.filter(element => element.Name === event.target.parentNode.parentNode.children[1].innerHTML.slice(7)).length;
-
-
-  event.target.parentNode.parentNode.children[5].children[1].innerHTML = numberOfUniqueItemsInCart
-
-
-  if( numberOfUniqueItemsInCart === 0){
-  
-  event.target.parentNode.parentNode.className = ""
- event.target.parentNode.parentNode.innerHTML = ""
  
  
-  
-}
-
-return numberOfUniqueItemsInCart
-}
-
-numberOfUniqueItemsInCart()
-     
-setTimeout(()=>{
-  cartQuantityDisplay.innerHTML = `(${JSON.parse(localStorage.getItem("itemsInCart")).length})`
-}, 100)
- 
-   
-  
- if(obj.Available > -1){
-  
-   event.target.parentNode.children[3].style.display = "none"
-   
-}
-
-  if(obj.Available > -1 ){
-
-  
-if(numberOfUniqueItemsInCart() === 0){
-
-localStorage.setItem("cartSum", +localStorage.getItem("cartSum") - obj.Price)
-
-  
-  return
-}
-
-addQuantityDirectly(`${objName}`)
-  
-
- localStorage.setItem("cartSum", +localStorage.getItem("cartSum") - obj.Price)
-
-  
 }
 
 
-if (dressIndex !== -1) {
-  itemsFromCart.splice(dressIndex, 1);
-  localStorage.setItem("itemsInCart", JSON.stringify(itemsFromCart))
- 
+
+export function removeFromCart(){
+
+ document.querySelectorAll(".removeItemInCartBtn, .removeBtn").forEach(function(btn){
+
+  btn.addEventListener("click" ,async function(event){
+
+    let buttonClicked = btn
+    let dressPriceDatabase = await getitemPrice(`${btn.title}`)
+    let cartItems = JSON.parse(localStorage.getItem("itemsInCart"))
+      
+      
+      if(event.target.className === "removeBtn"){
+        
+        let productContainer = btn.closest(".product__container2")
+        productContainer.remove()
+        
+        let sameDressesInCart = cartItems.filter(element => element.Name ===  btn.title);
+        addQuantityDirectlyMultipleItems(`${btn.title}`, sameDressesInCart.length)
+
+        let updatedCart = cartItems.filter(element => element.Name !== btn.title);
+        localStorage.setItem("itemsInCart", JSON.stringify(updatedCart));
+
+       updateCartIconQuantity()
+       subtractSumFromCart(dressPriceDatabase, sameDressesInCart.length )
+       ifEmptyCart()
+       
+      }
+
+
+    if(event.target.className === "removeItemInCartBtn"){
+
+    let dressToRemove = `${btn.title}`;
+    let firstDressOccurance = cartItems.findIndex(item => item.Name === dressToRemove);
+
+
+    console.log(+localStorage.getItem("cartSum"))
+
+      if(+localStorage.getItem("cartSum") === 0){
+         ifEmptyCart()
+      }
+
+      if (firstDressOccurance === -1 ){
+
+        let productContainer = btn.closest(".product__container2")
+        productContainer.remove()
+
+      }
+
+
+      if (firstDressOccurance !== -1) {
   
+        cartItems.splice(firstDressOccurance, 1);
+        localStorage.setItem("itemsInCart", JSON.stringify(cartItems));
+        addQuantityDirectlyMultipleItems(`${btn.title}`, 1)
+        updateCartIconQuantity()
+        subtractSumFromCart(dressPriceDatabase, 1 )
+        ifEmptyCart()
+        numberDisplayItemsInCart(buttonClicked)
+        dressOutOfStockCartHide( btn)
+
+
+let isAtLeastOneDressInCart = cartItems.some(item => item.Name === `${btn.title}`);
+
+
+      if(!isAtLeastOneDressInCart){
+  
+        let productContainer = btn.closest(".product__container2")
+        productContainer.remove()
+
+      }
+
+}}})})
+
 }
 
 
-document.querySelector(".order___sum").innerHTML = `Total: ${localStorage.getItem("cartSum")},00$`
-
-itemsFromCart = JSON.parse(localStorage.getItem("itemsInCart"))
-
-
-numberOfUniqueItemsInCart()
-
-
-if(+localStorage.getItem("cartSum") === 0){
-  
-    
-    document.querySelector(".order___sum").innerHTML = ""
-
-    document.getElementById("hero2").innerHTML = `<p>It's pretty empty here.</p>
-                                                    <a href="./reservation.html">
-  Let yourself be inspired</a>`
-
-  document.getElementById("checkoutBtn").style.display ="none"
-document.getElementById("cartQuantity").style.display ="none"
-}
-  
-  })
-   
-  }) 
-
-}
 
 displayProducts()
 
